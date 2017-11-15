d58bebe3cbed6bd550c1fc914f56dc2c8a58791c
Kriself <arsis7@mail.ru>
2017-11-15 18:25:12 +0300

    v3

--- js/jsMainPage.js
+++ js/jsMainPage.js
@@ -64,19 +64,19 @@ close.addEventListener('click', function (evt){
 //конец Напишите нам
 
 //Все категории меню
-var categories = document.querySelector('.categories');
-var allCat = document.querySelector('.allCat');
-var submenu =document.querySelector('.sub-menu');
-categories.addEventListener('mouseover', function (evt) {
-    evt.preventDefault();
-    submenu.style.height='200px';
-    allCat.classList.add('show');
-});
-categories.addEventListener('mouseleave', function (evt) {
-    evt.preventDefault();
-    submenu.style.height='83px';
-    allCat.classList.remove('show');
-});
+// var categories = document.querySelector('.categories');
+// var allCat = document.querySelector('.allCat');
+// var submenu =document.querySelector('.sub-menu');
+// categories.addEventListener('mouseover', function (evt) {
+//     evt.preventDefault();
+//     submenu.style.height='200px';
+//     allCat.classList.add('show');
+// });
+// categories.addEventListener('mouseleave', function (evt) {
+//     evt.preventDefault();
+//     submenu.style.height='83px';
+//     allCat.classList.remove('show');
+// });
 //конец Все категории меню
 
 var catItem1=document.querySelector('.catItem1');
